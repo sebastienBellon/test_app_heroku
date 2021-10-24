39ee9753ea2cbbeafe8d6b03115578a8316ff373
Sébastien Bellon <seb.bellon@protonmail.com>
2021-10-24 18:27:02 +0200

    correct PSQLURI env var from heroku

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
-database_path = os.environ['DATABASE_URL']
+#database_path = os.environ['DATABASE_URL']
+database_path = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 db = SQLAlchemy()
 
